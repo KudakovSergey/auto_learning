8c5ba86f10489f53bf16e29001a9fe524ac80a68
Sergey <nayrumi1994@gmail.com>
2023-01-27 17:02:56 +0300

    update elements test added class: - TestButtonPage added test: - click_on_different_button

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import random
 import time
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage
 
 
 class TestElements:
@@ -15,67 +15,81 @@ class TestElements:
             assert current_address == output_cur_addr, "the current_address does not match"
             assert permanent_address == output_per_addr, "the permanent_address does not match"
 
-        class TestCheckBox:
-            def test_check_box(self, driver):
-                check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
-                check_box_page.open()
-                check_box_page.open_full_list()
-                check_box_page.click_random_checkbox()
-                input_checkbox = check_box_page.get_checked_checkboxes()
-                output_result = check_box_page.get_output_result()
-                assert input_checkbox == output_result, 'checkboxes have not been selected'
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, 'checkboxes have not been selected'
 
-        class TestRadioButton:
-            def test_radio_button(self, driver):
-                radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
-                radio_button_page.open()
-                radio_button_page.click_on_the_radio_button('yes')
-                output_yes = radio_button_page.get_output_result()
-                radio_button_page.click_on_the_radio_button('impressive')
-                output_impressive = radio_button_page.get_output_result()
-                radio_button_page.click_on_the_radio_button('no')
-                output_no = radio_button_page.get_output_result()
-                assert output_yes == 'Yes', "'Yes' have not been selected"
-                assert output_impressive == 'Impressive', "'Impressive' have not been selected"
-                assert output_no == 'no', "'Yes' have not been selected"
+    class TestRadioButton:
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
+            radio_button_page.open()
+            radio_button_page.click_on_the_radio_button('yes')
+            output_yes = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('impressive')
+            output_impressive = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('no')
+            output_no = radio_button_page.get_output_result()
+            assert output_yes == 'Yes', "'Yes' have not been selected"
+            assert output_impressive == 'Impressive', "'Impressive' have not been selected"
+            assert output_no == 'no', "'Yes' have not been selected"
 
-        class TestWebTable:
-            def test_web_table_add_person(self, driver):
-                web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
-                web_table_page.open()
-                new_person = web_table_page.add_new_person()
-                table_result = web_table_page.check_new_added_person()
-                assert new_person in table_result
+    class TestWebTable:
+        def test_web_table_add_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            table_result = web_table_page.check_new_added_person()
+            assert new_person in table_result
 
-            def test_web_table_search_person(self, driver):
-                web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
-                web_table_page.open()
-                key_word = web_table_page.add_new_person()[random.randint(0, 5)]
-                web_table_page.search_some_person(key_word)
-                table_result = web_table_page.check_search_person()
-                assert key_word in table_result, "the person was not found"
+        def test_web_table_search_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            key_word = web_table_page.add_new_person()[random.randint(0, 5)]
+            web_table_page.search_some_person(key_word)
+            table_result = web_table_page.check_search_person()
+            assert key_word in table_result, "the person was not found"
 
-            def test_web_table_update_person_info(self, driver):
-                web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
-                web_table_page.open()
-                lastname = web_table_page.add_new_person()[1]
-                web_table_page.search_some_person(lastname)
-                age = web_table_page.update_person_info()
-                row = web_table_page.check_search_person()
-                assert age in row, "the person card has not been changed"
+        def test_web_table_update_person_info(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            lastname = web_table_page.add_new_person()[1]
+            web_table_page.search_some_person(lastname)
+            age = web_table_page.update_person_info()
+            row = web_table_page.check_search_person()
+            assert age in row, "the person card has not been changed"
 
-            def test_web_table_delete_person(self, driver):
-                web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
-                web_table_page.open()
-                email = web_table_page.add_new_person()[3]
-                web_table_page.search_some_person(email)
-                web_table_page.delete_person()
-                text = web_table_page.check_deleted()
-                assert text == 'No rows found'
+        def test_web_table_delete_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            email = web_table_page.add_new_person()[3]
+            web_table_page.search_some_person(email)
+            web_table_page.delete_person()
+            text = web_table_page.check_deleted()
+            assert text == 'No rows found'
 
-            def test_web_table_change_count_row(self, driver):
-                web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
-                web_table_page.open()
-                count = web_table_page.select_up_to_some_rows()
-                assert count == [5, 10, 20, 25, 50, 100], "The number of rows in the table has not been changed or " \
-                                                          "has changed incorrectly"
+        def test_web_table_change_count_row(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            web_table_page.remove_footer()
+            web_table_page.remove_fixedban()
+            count = web_table_page.select_up_to_some_rows()
+            assert count == [5, 10, 20, 25, 50, 100], "The number of rows in the table has not been changed or " \
+                                                      "has changed incorrectly"
+
+    class TestButtonsPage:
+
+        def test_different_click_on_the_buttons(self, driver):
+            button_page = ButtonsPage(driver, 'https://demoqa.com/buttons')
+            button_page.open()
+            double = button_page.click_on_different_button('double')
+            right = button_page.click_on_different_button('right')
+            click = button_page.click_on_different_button('click')
+            assert double == "You have done a double click", "The double click button was not pressed"
+            assert right == "You have done a right click", "The right click button was not pressed"
+            assert click == "You have done a dynamic click", "The click click button was not pressed"
