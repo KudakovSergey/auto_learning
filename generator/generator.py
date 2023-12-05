ade7dfe4c191fa24a4b00a90fe1859e84edc1662
Nayrumi <s.kudakov@apptimizm.pro>
2023-12-05 13:40:44 +0300

    test

---
+++ generator/generator.py
@@ -0,0 +1,21 @@
+import random
+
+from data.data import Person
+from faker import Faker
+
+faker_ru = Faker('ru_RU')
+Faker.seed()
+
+
+def generated_person():
+    yield Person(
+        full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        first_name=faker_ru.first_name(),
+        last_name=faker_ru.last_name(),
+        age=random.randint(10, 80),
+        department=faker_ru.job(),
+        salary=random.randint(10000, 90000),
+        email=faker_ru.email(),
+        current_address=faker_ru.address(),
+        permanent_address=faker_ru.address()
+    )
